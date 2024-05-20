4f5ae2ba073e255fe2570630d3b296abd9ff371f
jaanli <jaan.li+github.com@jaan.li>
2024-05-20 12:59:15 -0400

    wip: debug sftp

--- data_processing/scripts/download_opencorporates.com.sh
+++ data_processing/scripts/download_opencorporates.com.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-sshpass -p "${SFTP_PASSWORD}" sftp -q -P "${SFTP_PORT}" "${SFTP_USERNAME}@${SFTP_ENDPOINT}" <<EOF
-lcd .
-ls -l .
-bye
-EOF | grep -v "^sftp>" | awk '$1 ~ /^-/ {print $NF}'
+while IFS= read -r line; do
+    file=$(echo "$line" | awk '{print $2}')
+    sshpass -p "${SFTP_PASSWORD}" sftp -q -P "${SFTP_PORT}" "${SFTP_USERNAME}@${SFTP_ENDPOINT}" <<EOF
+    lcd .
+    cd tartu_uni/2024-05-20
+    get "$file"
+    bye
+EOF
+done < md5sum.txt
